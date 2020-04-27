eae269b957d1990fe628b919996949f9d6d9504d
Jonathan Ott <jonathan.ott1994@gmail.com>
2020-04-27 19:50:34 +0200

    Refactor ProteinInferenceVis.fs

--- src/BioFSharp.Mz.Vis/ProteinInferenceVis.fs
+++ src/BioFSharp.Mz.Vis/ProteinInferenceVis.fs
@@ -6,49 +6,57 @@ open BioFSharp.Mz.ProteinInference
 module ProteinInference =
 
     let qValueHitsVisualization bandwidth inferredProteinClassItemScored path (groupFiles: bool) =
-        let decoy, target = inferredProteinClassItemScored |> Array.partition (fun x -> x.DecoyBigger)
+        let decoy, target =
+            inferredProteinClassItemScored
+            |> Array.partition (fun x -> x.DecoyBigger)
         // Histogram with relative abundance
-        let freqTarget = 
+        let relFreqTarget = 
             FSharp.Stats.Distributions.Frequency.create bandwidth (target |> Array.map (fun x -> x.TargetScore))
             |> Map.toArray
             |> Array.map (fun x -> fst x, (float (snd x)) / (float target.Length))
-        let freqDecoy  = FSharp.Stats.Distributions.Frequency.create bandwidth (decoy |> Array.map (fun x -> x.DecoyScore))
-                            |> Map.toArray
-                            |> Array.map (fun x -> fst x, (float (snd x)) / (float target.Length))
+        let relFreqDecoy  =
+            FSharp.Stats.Distributions.Frequency.create bandwidth (decoy |> Array.map (fun x -> x.DecoyScore))
+            |> Map.toArray
+            |> Array.map (fun x -> fst x, (float (snd x)) / (float target.Length))
         // Histogram with absolute values
-        let freqTarget1 = FSharp.Stats.Distributions.Frequency.create bandwidth (target |> Array.map (fun x -> x.TargetScore))
-                            |> Map.toArray
-        let freqDecoy1  = FSharp.Stats.Distributions.Frequency.create bandwidth (decoy |> Array.map (fun x -> x.DecoyScore))
-                            |> Map.toArray
+        let absFreqTarget =
+            FSharp.Stats.Distributions.Frequency.create bandwidth (target |> Array.map (fun x -> x.TargetScore))
+            |> Map.toArray
+        let absFreqDecoy  =
+            FSharp.Stats.Distributions.Frequency.create bandwidth (decoy |> Array.map (fun x -> x.DecoyScore))
+            |> Map.toArray
         let histogram =
             [
-                Chart.Column freqTarget |> Chart.withTraceName "Target"
-                    |> Chart.withAxisAnchor(Y=1);
-                Chart.Column freqDecoy |> Chart.withTraceName "Decoy"
-                    |> Chart.withAxisAnchor(Y=1);
-                Chart.Column freqTarget1
-                    |> Chart.withAxisAnchor(Y=2)
-                    |> Chart.withMarkerStyle (Opacity = 0.)
-                    |> Chart.withTraceName (Showlegend = false);
-                Chart.Column freqDecoy1
-                    |> Chart.withAxisAnchor(Y=2)
-                    |> Chart.withMarkerStyle (Opacity = 0.)
-                    |> Chart.withTraceName (Showlegend = false)
+                Chart.Column relFreqTarget
+                |> Chart.withTraceName "Target"
+                |> Chart.withAxisAnchor(Y=1);
+                Chart.Column relFreqDecoy
+                |> Chart.withTraceName "Decoy"
+                |> Chart.withAxisAnchor(Y=1);
+                Chart.Column absFreqTarget
+                |> Chart.withAxisAnchor(Y=2)
+                |> Chart.withMarkerStyle (Opacity = 0.)
+                |> Chart.withTraceName (Showlegend = false);
+                Chart.Column absFreqDecoy
+                |> Chart.withAxisAnchor(Y=2)
+                |> Chart.withMarkerStyle (Opacity = 0.)
+                |> Chart.withTraceName (Showlegend = false)
             ]
             |> Chart.Combine
 
         let sortedQValues =
             inferredProteinClassItemScored
-            |> Array.map
-                (fun x -> if x.Decoy then
-                            x.DecoyScore, x.QValue
-                            else
-                            x.TargetScore, x.QValue
-                )
+            |> Array.map (fun x ->
+                if x.Decoy then
+                    x.DecoyScore, x.QValue
+                else
+                    x.TargetScore, x.QValue
+            )
             |> Array.sortBy (fun (score, qVal) -> score)
 
         [
-            Chart.Point sortedQValues |> Chart.withTraceName "Q-Values";
+            Chart.Point sortedQValues
+            |> Chart.withTraceName "Q-Values";
             histogram
         ]
         |> Chart.Combine
@@ -57,6 +65,6 @@ module ProteinInference =
         |> Chart.withX_AxisStyle "Score"
         |> Chart.withSize (900., 900.)
         |> if groupFiles then
-            Chart.SaveHtmlAs (path + @"\QValueGraph")
+               Chart.SaveHtmlAs (path + @"\QValueGraph")
            else
-            Chart.SaveHtmlAs (path + @"_QValueGraph")
+               Chart.SaveHtmlAs (path + @"_QValueGraph")
