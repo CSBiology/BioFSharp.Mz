916ec3303b80fdd53e490564996c35437e3d0065
Jonathan Ott <jonathan.ott1994@gmail.com>
2020-04-29 12:16:58 +0200

    Apply review changes

--- src/BioFSharp.Mz/FDRControl.fs
+++ src/BioFSharp.Mz/FDRControl.fs
@@ -39,34 +39,30 @@ module FDRControl =
 
         /// Estimates the false positives given the total number of entries, the number of target hits and the number of decoy hits
         let estimatePi0HG (n: float) (targets: float) (cf: float) =
-            let rec loop (fp: float) (logprob: float list) =
-                if fp > cf then
-                    logprob |> List.rev
-                else
+            let logprob = 
+                [0. .. cf]
+                |> List.fold (fun acc fp ->
                     let tp = targets - fp
                     let w = n - tp
                     let prob = hypergeometric fp n w cf
-                    loop (fp + 1.) (prob::logprob)
-            let logprob = loop 0. []
+                    prob::acc
+                ) []
+                |> List.rev
             let sum = logprob |> List.sum
             let logprob_Norm =
                 logprob
-                |> List.map (fun x ->
-                    x / sum
-                )
+                |> List.map (fun x -> x / sum)
             // MAYU rounds here to first decimal
             let expectation_value_FP_PID =
                 logprob_Norm
-                |> List.foldi (fun i acc x ->
-                    acc + x * (float i)
-                ) 0.
+                |> List.foldi (fun i acc x -> acc + x * (float i)) 0.
             if (isNan expectation_value_FP_PID) || (isInf expectation_value_FP_PID) then
                 0.
             else
                 expectation_value_FP_PID
 
     /// for given data, creates a logistic regression model and returns a mapping function for this model
-    let getLogisticRegressionFunction (x:vector) (y:vector) epsilon = 
+    let initLogisticRegressionFunction (x:vector) (y:vector) epsilon = 
         let alpha = 
             match FSharp.Stats.Fitting.LogisticRegression.Univariable.estimateAlpha epsilon x y with 
             | Some a -> a
@@ -77,8 +73,14 @@ module FDRControl =
     /// returns scores, pep, q
     let binningFunction bandwidth pi0 (scoreF: 'A -> float) (isDecoyF: 'A -> bool) (data:'A[])  = 
         let totalDecoyProportion = 
-            let decoyCount = Array.filter isDecoyF data |> Array.length |> float
-            let totalCount = data |> Array.length  |> float
+            let decoyCount = 
+                Array.filter isDecoyF data
+                |> Array.length
+                |> float
+            let totalCount =
+                data
+                |> Array.length
+                |> float
             1. / (2. * decoyCount / totalCount)
         data
         |> Array.groupBy (fun s -> floor (scoreF s / bandwidth))
@@ -87,9 +89,7 @@ module FDRControl =
             let median     = values |> Array.map scoreF |> Array.average
             let totalCount = values |> Array.length |> float
             let decoyCount = values |> Array.filter isDecoyF |> Array.length |> float |> (*) totalDecoyProportion
-            //(median |> float,(decoyCount * pi0  / totalCount))
             median,totalCount,decoyCount
-                //(median, totalCount )
         )
         |> fun a ->
             a
@@ -103,31 +103,31 @@ module FDRControl =
         |> fun (score,pep,q) -> vector score, vector pep, vector q
     
     /// Calculates q value mapping funtion for target/decoy dataset
-    let getQValueFunc pi0 (scoreF: 'A -> float) (isDecoyF: 'A -> bool) (data:'A[]) = 
+    let initQValueFunc pi0 (scoreF: 'A -> float) (isDecoyF: 'A -> bool) (data:'A[]) = 
         let bw = 
             data 
             |> Array.map scoreF
             |> FSharp.Stats.Distributions.Bandwidth.nrd0
         let (scores,_,q) = binningFunction bw pi0 scoreF isDecoyF data
-        getLogisticRegressionFunction scores q 0.0000001
+        initLogisticRegressionFunction scores q 0.0000001
 
     /// Calculates q values for target/decoy dataset
     let getQValues pi0 (scoreF: 'A -> float) (isDecoyF: 'A -> bool) (data:'A[]) = 
-        let f = getQValueFunc pi0 scoreF isDecoyF data
+        let f = initQValueFunc pi0 scoreF isDecoyF data
         Array.map (scoreF >> f) data
 
     /// Calculates pep value mapping funtion for target/decoy dataset
-    let getPEPValueFunc pi0 (scoreF: 'A -> float) (isDecoyF: 'A -> bool) (data:'A[]) = 
+    let initPEPValueFunc pi0 (scoreF: 'A -> float) (isDecoyF: 'A -> bool) (data:'A[]) = 
         let bw = 
             data 
             |> Array.map scoreF
             |> FSharp.Stats.Distributions.Bandwidth.nrd0
         let (scores,pep,_) = binningFunction bw pi0 scoreF isDecoyF data
-        getLogisticRegressionFunction scores pep 0.0000001
+        initLogisticRegressionFunction scores pep 0.0000001
 
     /// Calculates pep values for target/decoy dataset
     let getPEPValues pi0 (scoreF: 'A -> float) (isDecoyF: 'A -> bool) (data:'A[]) = 
-        let f = getPEPValueFunc pi0 scoreF isDecoyF data 
+        let f = initPEPValueFunc pi0 scoreF isDecoyF data 
         Array.map (scoreF >> f) data 
     
     /// Calculates Q-Values from pep-values
